09177d3b317706ade22a41acb6a915892f9f4a55
hvupham <21522804@gm.uit.edu.vn>
2024-10-18 21:06:06 +0700

    fix UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,18 @@
-import { NgModule, importProvidersFrom } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { OrderDetailComponent } from './components/detail-order/order.detail.component';
+import {
+  DetailProductComponent
+} from './components/detail-product/detail-product.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { 
-  DetailProductComponent 
-} from './components/detail-product/detail-product.component';
-import { OrderComponent } from './components/order/order.component';
-import { OrderDetailComponent } from './components/detail-order/order.detail.component';
-import { UserProfileComponent } from './components/user-profile/user.profile.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { AuthGuardFn } from './guards/auth.guard';
-import { AdminGuardFn } from './guards/admin.guard';
-import { CategoriesComponent } from './components/categories/categories.component';
 import { MyOrderedComponent } from './components/my-ordered/my-ordered.component';
+import { OrderComponent } from './components/order/order.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserProfileComponent } from './components/user-profile/user.profile.component';
+import { AdminGuardFn } from './guards/admin.guard';
+import { AuthGuardFn } from './guards/auth.guard';
 //import { OrderAdminComponent } from './components/admin/order/order.admin.component';
 import { UpdateUserComponent } from './components/user-profile/update-user/update.profile.component';
 export const routes: Routes = [
@@ -29,9 +27,6 @@ export const routes: Routes = [
   { path: 'my-ordered', component: MyOrderedComponent, canActivate:[AuthGuardFn] },
   { path: 'my-ordered/:id', component: OrderDetailComponent, canActivate:[AuthGuardFn] },
   { path: 'users/update', component: UpdateUserComponent },
-
-
-
   //Admin   
   { 
     path: 'admin', 
